72d36da69f8b062237a197ccb7e305b2fe26f22e
Corpulent Brony <corpulent@brony.email>
2026-04-26 12:14:41 -0400

    moved common values that change by year to consts.ts

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+export const YEAR: number = 2026;
+
+export const NAME: string = `/mlp/ con ${YEAR}`;
+export const DATES: string = `July 10-12, ${YEAR}`;
+export const DATES_ISO: string = `${YEAR}-07-10/P3D`;
