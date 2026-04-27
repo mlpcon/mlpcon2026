83b8a9cf575e32ba8842391a4aafc1a3a8c5e629
Corpulent Brony <corpulent@brony.email>
2026-04-26 23:07:29 -0400

    undoing test

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,3 @@ export const YEAR: number = 2026;
 export const NAME: string = `/mlp/ con ${YEAR}`;
 export const DATES: string = `July 10-12, ${YEAR}`;
 export const DATES_ISO: string = `${YEAR}-07-10/P3D`;
-const test = 0;
